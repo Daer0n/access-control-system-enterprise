f04a01bfa72eb231603a99dc72335717ec79763f
Daer0n <danilatarbaya57@gmail.com>
2023-12-12 23:48:12 +0300

    refactor(models/user): add column for auth

--- backend/src/models/users/user.py
+++ backend/src/models/users/user.py
@@ -1,11 +1,12 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declared_attr
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+
 
 from database.database import Base
 
 
-class User(Base):
+class User(SQLAlchemyBaseUserTable[int],Base):
     __tablename__ = 'User'
     __abstract__ = True
 
@@ -13,3 +14,15 @@ class User(Base):
     name = Column(String, nullable=False)
     email = Column(String, nullable=False)
     password = Column(String, nullable=False)
+
+    @declared_attr
+    def is_active(cls):
+        return Column(Boolean, default=True, nullable=False)
+
+    @declared_attr
+    def is_superuser(cls):
+        return Column(Boolean, default=False, nullable=False)
+
+    @declared_attr
+    def is_verified(cls):
+        return Column(Boolean, default=False, nullable=False)
