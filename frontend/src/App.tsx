ddf224948a8c4b33c1ff1ebd341c8936543a81ad
Daer0n <danilatarbaya57@gmail.com>
2023-12-17 02:36:19 +0300

    feat(frontend): create react app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <div className="App-logo">Access control system enterprise</div>
+        <div className="button-group">
+          <button className="btn btn-primary btn-lg mr-5">Log in</button>
+          <button className="btn btn-primary btn-lg">Log out</button>
+        </div>
+      </nav>
+
+      <div></div>
+    </div>
+  );
+}
+
+export default App;
